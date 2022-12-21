079bcab335c6b27196cbc42688600b2c2a0e25c6
Lakshmiprabhaa2812 <121005925+Lakshmiprabhaa2812@users.noreply.github.com>
2022-12-21 10:09:57 +0530

    Create variables.tf

---
+++ trainingdocs/ecs/ecs_mainmodule/variables.tf
@@ -0,0 +1,137 @@
+# AWS Launch Configuration
+
+variable "image_id" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "security_groups" {
+    type = list(string)
+}
+
+# Auto Scaling Group
+
+variable "min_size" {
+    type = number
+}
+
+variable "max_size" {
+    type = number
+}
+
+variable "vpc_zone_identifier" {
+    type = list(string)
+}
+
+# ECS Cluster
+
+variable "ecs_cluster_name" {
+  type        = string
+}
+
+# ECS Task Definition
+
+variable "ecs_task_definition_name" {
+  type        = string
+}
+
+variable "container_definitions" {
+   type       = any
+}
+
+# ECS Service Creation
+
+variable "ecs_service_name" {
+  type        = string
+}
+
+variable "launch_type" {
+  type        = string
+}
+
+variable "desired_count" {
+  type        = number
+}
+
+variable "container_name" {
+  type        = string
+}
+
+variable "container_port" {
+  type        = number
+}
+
+variable "subnets" {
+  type        = list(string)
+}
+/*
+variable "task_definition" {
+  type        = string
+}
+*/
+# ALB
+
+variable "name" {
+  type = string
+}
+
+variable "internal" {
+  type = string
+}
+
+variable "load_balancer_type" {
+  type = string
+}
+
+variable "lb_security_groups" {
+  type = list(any)
+}
+
+variable "enable_deletion_protection" {
+  type = bool
+}
+
+variable "lb_subnets" {
+  type = list(string)
+}
+
+# Target Groups
+
+variable "target_group_name" {
+  type = string
+}
+
+variable "protocol" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "port" {
+  type = number
+}
+
+# Listener
+
+variable "listener_port" {
+  type = number
+}
+
+variable "listener_protocol" {
+  type = string
+}
+
+# Load Balancer Listerner Rule
+
+variable "listener_priority" {
+  type = number
+}
+
+variable "path_pattern_match" {
+  type = string
+}
